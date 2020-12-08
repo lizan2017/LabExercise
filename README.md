02f4e0d7d647e0f856bf5cacb881356fdeae10b4
Lizan <lizan.pra19@gmail.com>
2020-12-08 22:20:28 +0545

    lab exercise commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Quiz-App
+Harry Potter quiz app created using android studio
+
+After completing the basic Android course from Udacity, I developed the Harry Potter Quiz App. Using intents and the basic concepts 
---
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.quiz;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startQuiz(View v)
+    {
+        Intent intent = new Intent(this, Question1.class);
+
+        startActivity(intent);
+    }
+}
